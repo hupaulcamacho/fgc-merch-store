33eddfbb979d8d234a075726cf4dea48cb29466c
traplordhuey <hupaulcamacho03@gmail.com>
2022-10-02 23:51:06 -0400

    import react router dom and set up 3 basic routes for testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
+import React from 'react'
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path='/checkout' element={<h1> Checkout </h1>} />
+          <Route path='/login' element={<h1> Login </h1>} />
+          <Route path='/' element={<h1> Homepage </h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
