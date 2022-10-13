ae1a9c2d872cb713bb990f578f4287eb2e9736c4
traplordhuey <hupaulcamacho03@gmail.com>
2022-10-12 21:48:32 -0400

    install material ui and create header component with routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react"
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// component import
+import Header from "./Header";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <Routes>
-          <Route path='/checkout' element={<h1> Checkout </h1>} />
-          <Route path='/login' element={<h1> Login </h1>} />
-          <Route path='/' element={<h1> Homepage </h1>} />
+          <Route path="/checkout" element={<h1> Checkout </h1>} />
+          <Route path="/login" element={<h1> Login </h1>} />
+          <Route path="/" element={<><Header /><h1> Homepage </h1></>} />
         </Routes>
       </div>
     </BrowserRouter>
